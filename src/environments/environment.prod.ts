14f971d6c1afde53e2f6d181b0ef6f01a39033f3
Hariharnautiyal <101192066+Hariharnautiyal@users.noreply.github.com>
2023-03-05 11:23:46 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,22 +1,21 @@
 export const environment = {
   firebase: {
-  firebase: {
-apiKey: "AIzaSyAqu3-88917c199B-_ASZOYgkR5f73Dl_4",
-    
-authDomain: "chatwithharihar.firebaseapp.com",
+  apiKey: "AIzaSyAqu3-88917c199B-_ASZOYgkR5f73Dl_4",
 
-databaseURL: "https://chatwithharihar-default-rtdb.asia-southeast1.firebasedatabase.app",
+  authDomain: "chatwithharihar.firebaseapp.com",
 
-projectId: "chatwithharihar",
+  databaseURL: "https://chatwithharihar-default-rtdb.asia-southeast1.firebasedatabase.app",
 
-storageBucket: "chatwithharihar.appspot.com",
+  projectId: "chatwithharihar",
 
-messagingSenderId: "807318029739",
+  storageBucket: "chatwithharihar.appspot.com",
 
-appId: "1:807318029739:web:ee6a4a68e3052ce034feee",
+  messagingSenderId: "807318029739",
 
-measurementId: "G-BJFNNCYMG6"
-  },
+  appId: "1:807318029739:web:ee6a4a68e3052ce034feee",
+
+  measurementId: "G-BJFNNCYMG6"
+
   },
   production: true
 };
